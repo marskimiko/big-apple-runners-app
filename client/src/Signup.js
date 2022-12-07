538e35a7985296f188609f79a9de4452f31e42f4
marisasaunders <marisaksaunders@gmail.com>
2022-12-07 14:09:10 -0500

    build handleSubmit function in signup component)

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,15 +1,37 @@
 import React, { useState, useContext } from 'react'
 import { UserContext } from "./context/user";
+import { useNavigate } from 'react-router-dom';
 
 function Signup() {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
   const [passwordConfirmation, setPasswordConfirmation] = useState("")
-  const [errorsList setErrorsList] =useState([])
-  const [signup] = useContext(UserContext);
+  const [errorsList, setErrorsList] =useState([])
+  const {signup} = useContext(UserContext);
+  const navigate = useNavigate();
 
-  const handleSubmit= (e) => {
-    e.preventDefault
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetch('/signup', {
+      method: 'POST',
+      headers: { 'Content-Type' : 'application/json'},
+      body: JSON.stringify({
+        username: username,
+        password: password,
+        password_confirmation: passwordConfirmation 
+      })
+    })
+    .then(res => res.json())
+    .then(user => {
+      if (!user.errors) {
+        signup(user)
+        navigate('/');
+      } else {
+        setUsername("")
+        setPassword("")
+        setPasswordConfirmation("")
+      }
+    })
   }
 
   return (
@@ -20,7 +42,7 @@ function Signup() {
             type="text"
             id="name"
             value={username}
-            onChange={(e) => setName(e.target.value)}
+            onChange={(e) => setUsername(e.target.value)}
           /> 
         <br/>
         <label>Password: </label>
