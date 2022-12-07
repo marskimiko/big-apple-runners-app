6a7bd854b9d28e54db8b245e530585463d96da9e
marisasaunders <marisaksaunders@gmail.com>
2022-12-07 14:31:44 -0500

    add @current user to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,18 @@ class ApplicationController < ActionController::API
 
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
+  before_action :authorize
+
   private
 
-  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+  end
+
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,17 @@
 class UsersController < ApplicationController
 
+  skip_before_action :authorize, only: :create
+
   #signup
   def create
     user = User.create!(user_params)
-    if user.valid?
-      # user logging in by adding user to the session
-      session[:user_id] = user.id
-      render json: user
-    else
-      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    session[:user_id] = user.id
+    render json: user, status: :created
   end
 
    # get current user and render in in json
   def show
-    user = User.find_by(id: session[:user_id])
-    if user
-      render json: user
-    else
-      render json: { error: "Not Authorized" }, status: :unauthorized
-    end
+    render json: @current_user
   end
 
   private
